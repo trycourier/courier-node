b93ca96718c0872d4923aa8ac4dcb5cdc35eb441
Aydrian Howard <aydrian@gmail.com>
2020-08-17 09:11:16 -0400

    Added tests for topics api

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -2,7 +2,11 @@ import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
 import { CourierClient } from "../index";
 
-import { ICourierSendResponse } from "../types";
+import {
+  ICourierSendPatternParams,
+  ICourierSendResponse,
+  ICourierSendTopicParams
+} from "../types";
 
 const mockSendResponse: ICourierSendResponse = {
   messageId: "1234"
@@ -12,6 +16,7 @@ describe("CourierClient", () => {
   let mock: MockAdapter;
   beforeEach(() => {
     mock = new MockAdapter(axios);
+    mock.onPost("/send/topic").reply(200, mockSendResponse);
     mock.onPost("/send").reply(200, mockSendResponse);
   });
 
@@ -59,5 +64,64 @@ describe("CourierClient", () => {
         idempotencyKey: "IDEMPOTENCY_KEY_UUID"
       }
     );
+  });
+
+  test(".sendTopicOrPattern with Topic", async () => {
+    const { sendTopicOrPattern } = CourierClient({
+      authorizationToken: "AUTH_TOKEN"
+    });
+
+    const params: ICourierSendTopicParams = {
+      data: {
+        example: "EXAMPLE_DATA"
+      },
+      eventId: "EVENT_ID",
+      topic: "example.topic.id"
+    };
+
+    await expect(sendTopicOrPattern(params)).resolves.toMatchObject(
+      mockSendResponse
+    );
+  });
+
+  test(".sendTopicOrPattern with Pattern", async () => {
+    const { sendTopicOrPattern } = CourierClient({
+      authorizationToken: "AUTH_TOKEN"
+    });
+
+    const params: ICourierSendPatternParams = {
+      data: {
+        example: "EXAMPLE_DATA"
+      },
+      eventId: "EVENT_ID",
+      pattern: "example.topic.*"
+    };
+
+    await expect(sendTopicOrPattern(params)).resolves.toMatchObject(
+      mockSendResponse
+    );
+  });
+
+  test(".sendTopicOrPattern with Idempotency Key", async () => {
+    const { sendTopicOrPattern } = CourierClient({
+      authorizationToken: "AUTH_TOKEN"
+    });
+
+    mock.onPost("/send").reply(async (config) => {
+      expect(config.headers["Idempotency-Key"]).toBe("IDEMPOTENCY_KEY_UUID");
+      return [200];
+    });
+
+    const params: ICourierSendTopicParams = {
+      data: {
+        example: "EXAMPLE_DATA"
+      },
+      eventId: "EVENT_ID",
+      topic: "example.topic.id"
+    };
+
+    await sendTopicOrPattern(params, {
+      idempotencyKey: "IDEMPOTENCY_KEY_UUID"
+    });
   });
 });
--- src/__tests__/profiles.test.ts
+++ src/__tests__/profiles.test.ts
@@ -3,6 +3,7 @@ import MockAdapter from "axios-mock-adapter";
 import { CourierClient } from "../index";
 
 import {
+  ICourierProfileGetRecipientTopicsResponse,
   ICourierProfileGetResponse,
   ICourierProfilePostResponse,
   ICourierProfilePutResponse
@@ -22,12 +23,27 @@ const mockGetProfileResponse: ICourierProfileGetResponse = {
   }
 };
 
+const mockGetRecipientTopicsResponse: ICourierProfileGetRecipientTopicsResponse = {
+  paging: { more: false },
+  results: [
+    {
+      created: 1591814489093,
+      id: "example.topic.id",
+      name: "Example Topic",
+      updated: 1591814489143
+    }
+  ]
+};
+
 describe("CourierProfiles", () => {
   let mock: MockAdapter;
   beforeEach(() => {
     mock = new MockAdapter(axios);
     mock.onPut(/\/profiles\/.*/).reply(200, mockReplaceProfileResponse);
     mock.onPost(/\/profiles\/.*/).reply(200, mockMergeProfileResponse);
+    mock
+      .onGet(/\/profiles\/.*\/topics/)
+      .reply(200, mockGetRecipientTopicsResponse);
     mock.onGet(/\/profiles\/.*/).reply(200, mockGetProfileResponse);
   });
 
@@ -94,5 +110,15 @@ describe("CourierProfiles", () => {
         recipientId: "RECIPIENT_ID"
       })
     ).resolves.toMatchObject(mockGetProfileResponse);
+  });
+
+  test(".getRecipientTopics", async () => {
+    const { profiles } = CourierClient({
+      authorizationToken: "AUTH_TOKEN"
+    });
+
+    await expect(
+      profiles.getRecipientTopics("RECIPIENT_ID")
+    ).resolves.toMatchObject(mockGetRecipientTopicsResponse);
   });
 });
