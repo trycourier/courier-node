e608acad21a013a59ad114abdab3d2b7f164e8f4
jrweingart <jrweingart@gmail.com>
2021-03-03 17:53:11 -0700

    Testing errors - no idempotent tests yet

--- src/__tests__/lists.test.ts
+++ src/__tests__/lists.test.ts
@@ -14,6 +14,8 @@ import {
   ICourierSendResponse
 } from "../types";
 
+const mockError = "Bad Request";
+
 const mockSendResponse: ICourierSendResponse = {
   messageId: "1234"
 };
@@ -210,3 +212,163 @@ describe("CourierLists", () => {
     ).resolves.toMatchObject(mockFindByRecipientIdResponse);
   });
 });
+
+describe("CourierLists", () => {
+  let mock: MockAdapter;
+  beforeEach(() => {
+    mock = new MockAdapter(axios);
+    // GET /lists
+    mock.onGet("/lists").reply(400, mockError);
+    // GET /lists/{list_id}/subscriptions
+    mock
+      .onGet(/\/lists\/.*\/subscriptions/)
+      .reply(400, mockError);
+    // GET /lists/{list_id}
+    mock.onGet(/\/lists\/.*/).abortRequest();
+    // PUT /lists/{list_id}/subscriptions
+    mock.onPut(/\/lists\/.*\/subscriptions/).networkError();
+    // PUT /lists/{list_id}/subscriptions/{recipient_id}
+    mock.onPut(/\/lists\/.*\/subscriptions\/.*/).networkErrorOnce();
+    // PUT /lists/{list_id}/restore
+    mock.onPut(/\/lists\/.*\/restore/).timeout();
+    // PUT /lists/{list_id}
+    mock.onPut(/\/lists\/.*/).abortRequest()
+    // DELETE /lists/{list_id}/subscriptions/{recipient_id}
+    mock.onDelete(/\/lists\/.*\/subscriptions\/.*/).reply(400, mockError);
+    // DELETE /lists/{list_id}
+    mock.onDelete(/\/lists\/.*/).reply(400, mockError);
+    // POST /send/list
+    mock.onPost("/send/list").reply(400, mockError);
+    // GET /profiles/{recipient_id}/lists
+    mock
+      .onGet(/\/profiles\/.*\/lists/)
+      .timeout();
+  });
+
+  test(".send with List", async () => {
+    const { lists } = CourierClient({
+      authorizationToken: "AUTH_TOKEN"
+    });
+
+    const params: ICourierSendListOrPatternParams = {
+      data: {
+        example: "EXAMPLE_DATA"
+      },
+      event: "EVENT_ID",
+      list: "example.list.id"
+    };
+
+    await expect(lists.send(params)).rejects.toThrowError(mockError);
+  });
+
+  test(".send with Pattern", async () => {
+    const { lists } = CourierClient({
+      authorizationToken: "AUTH_TOKEN"
+    });
+
+    const params: ICourierSendListOrPatternParams = {
+      data: {
+        example: "EXAMPLE_DATA"
+      },
+      event: "EVENT_ID",
+      pattern: "example.list.*"
+    };
+
+    await expect(lists.send(params)).rejects.toThrowError(mockError);
+  });
+
+  test(".list", async () => {
+    const { lists } = CourierClient({
+      authorizationToken: "AUTH_TOKEN"
+    });
+
+    await expect(lists.list()).rejects.toThrowError(mockError);
+  });
+
+  test(".get", async () => {
+    const { lists } = CourierClient({
+      authorizationToken: "AUTH_TOKEN"
+    });
+
+    await expect(lists.get("example.list.id")).rejects.toThrowError(mockError);
+  });
+
+  test(".put", async () => {
+    const { lists } = CourierClient({
+      authorizationToken: "AUTH_TOKEN"
+    });
+
+    await expect(
+      lists.put("example.list.id", {
+        name: "Updated Example List"
+      })
+    ).rejects.toThrowError(mockError);
+  });
+
+  test(".delete", async () => {
+    const { lists } = CourierClient({
+      authorizationToken: "AUTH_TOKEN"
+    });
+
+    await expect(lists.delete("example.list.id")).rejects.toThrowError(mockError);
+  });
+
+  test(".restore", async () => {
+    const { lists } = CourierClient({
+      authorizationToken: "AUTH_TOKEN"
+    });
+
+    await expect(lists.restore("example.list.id")).rejects.toThrowError(mockError);
+  });
+
+  test(".getSubscriptions", async () => {
+    const { lists } = CourierClient({
+      authorizationToken: "AUTH_TOKEN"
+    });
+
+    await expect(
+      lists.getSubscriptions("example.list.id")
+    ).rejects.toThrowError(mockError);
+  });
+
+  test(".putSubscriptions", async () => {
+    const { lists } = CourierClient({
+      authorizationToken: "AUTH_TOKEN"
+    });
+
+    await expect(
+      lists.putSubscriptions("example.list.id", [{ recipientId: "ABCD1234" }])
+    ).rejects.toThrowError(mockError);
+  });
+
+  test(".subscribe", async () => {
+    const { lists } = CourierClient({
+      authorizationToken: "AUTH_TOKEN"
+    });
+
+    await expect(
+      lists.subscribe("example.list.id", "ABCD1234")
+    ).rejects.toThrowError(mockError);
+  });
+
+  test(".unsubscribe", async () => {
+    const { lists } = CourierClient({
+      authorizationToken: "AUTH_TOKEN"
+    });
+
+    await expect(
+      lists.unsubscribe("example.list.id", "ABCD1234")
+    ).rejects.toThrowError(mockError);
+  });
+
+  test(".findByRecipientId", async () => {
+    const { lists } = CourierClient({
+      authorizationToken: "AUTH_TOKEN"
+    });
+
+    await expect(
+      lists.findByRecipientId("RECIPIENT_ID")
+    ).rejects.toThrowError(mockError);
+  });
+});
+
