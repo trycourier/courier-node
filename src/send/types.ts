073fbfb0d217d5ebe36a3d02f27190825221cdce
Alexander Guzman <alexander@courier.com>
2022-03-15 14:27:10 -0400

    Reverting to 3.6 TSC and removing clever typings

--- src/send/types.ts
+++ src/send/types.ts
@@ -384,19 +384,10 @@ export interface RoutingStrategyProvider<T = Record<string, any>> {
   if?: string;
 }
 
-type TupleOf<T, N extends number> = N extends N
-  ? number extends N
-    ? T[]
-    : _TupleOf<T, N, []>
-  : never;
-type _TupleOf<T, N extends number, R extends unknown[]> = R["length"] extends N
-  ? R
-  : _TupleOf<T, N, [T, ...R]>;
-type Tags<T> = TupleOf<T, 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9>;
 
 export interface MessageMetadata {
   event?: string;
-  tags?: Tags<string>;
+  tags?: Array<string>;
   utm?: {
     source?: string;
     medium?: string;
