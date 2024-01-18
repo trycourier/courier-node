12acca8e68a1084437a85f635638770850885888
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-01-18 22:17:45 +0000

    SDK regeneration

--- src/api/resources/users/resources/preferences/client/requests/UserPreferencesUpdateParams.ts
+++ src/api/resources/users/resources/preferences/client/requests/UserPreferencesUpdateParams.ts
@@ -6,7 +6,7 @@ import * as Courier from "../../../../../..";
 
 export interface UserPreferencesUpdateParams {
     status: Courier.PreferenceStatus;
-    /** Should contain unique items. */
+    /** The Channels a user has chosen to receive notifications through for this topic */
     custom_routing?: Courier.ChannelClassification[];
     default_status: Courier.PreferenceStatus;
     has_custom_routing?: boolean;
