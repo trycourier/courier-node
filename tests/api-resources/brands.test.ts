c33c3ad21aa8ffca501828b63296dd321b72ef7b
courier-codegen <codegen@users.noreply.github.com>
2026-09-04 23:44:03 +0000

    feat(api): document the expiry_date contract and allow boolean patch values

--- tests/api-resources/brands.test.ts
+++ tests/api-resources/brands.test.ts
@@ -30,7 +30,17 @@ describe('resource brands', () => {
       settings: {
         colors: { primary: '#9D3789', secondary: '#FFFFFF' },
         email: {
-          footer: { content: 'content', inheritDefault: true },
+          footer: {
+            inheritDefault: true,
+            markdown: 'markdown',
+            social: {
+              facebook: { url: 'url' },
+              instagram: { url: 'url' },
+              linkedin: { url: 'url' },
+              medium: { url: 'url' },
+              twitter: { url: 'url' },
+            },
+          },
           head: { inheritDefault: true, content: 'content' },
           header: {
             logo: { href: 'href', image: 'image' },
@@ -106,7 +116,17 @@ describe('resource brands', () => {
       settings: {
         colors: { primary: '#9D3789', secondary: '#FFFFFF' },
         email: {
-          footer: { content: 'content', inheritDefault: true },
+          footer: {
+            inheritDefault: true,
+            markdown: 'markdown',
+            social: {
+              facebook: { url: 'url' },
+              instagram: { url: 'url' },
+              linkedin: { url: 'url' },
+              medium: { url: 'url' },
+              twitter: { url: 'url' },
+            },
+          },
           head: { inheritDefault: true, content: 'content' },
           header: {
             logo: { href: 'href', image: 'image' },
